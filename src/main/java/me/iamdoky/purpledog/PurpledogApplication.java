68e11d8b12d290e54def627b43dc1bcee274c115
doky <48897815+iamdoky@users.noreply.github.com>
2022-03-30 00:11:41 +0900

    feat : purpledog project create

---
+++ src/main/java/me/iamdoky/purpledog/PurpledogApplication.java
@@ -0,0 +1,13 @@
+package me.iamdoky.purpledog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpledogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurpledogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/iamdoky/purpledog/PurpledogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iamdoky.purpledog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurpledogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
